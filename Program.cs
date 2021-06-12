3f2c13f2cf4c9c81f9913f66ac4526c66d83b3ea
ranabhinder1321 <ranabhinder@yahoo.co.nz>
2021-06-12 15:46:51 +1200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Greatest_User_Input_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, c;
+
+            Console.WriteLine("Please enter first number");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please enter second number");
+            b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please enter third number");
+            c = Convert.ToInt32(Console.ReadLine());
+
+            if (a > b & a > c)
+            {
+                Console.WriteLine("a is the gratest number");
+
+            }
+
+            else if (b > a & b > c)
+
+            {
+                Console.WriteLine("b is the gratest number");
+
+            }
+
+            else if (c > a & c > b)
+
+            {
+                Console.WriteLine("c is the gratest number");
+                Console.ReadLine();
+
+
+            }
+
+
+        }
+
+    }
+}
+
+
+
+
